09695b03070d363ad770854c88b7de681ddca382
Dan Do <dan.do@droppii.com>
2025-07-25 03:30:33 +0700

    init

--- apps/web/src/components/ChatDemo.tsx
+++ apps/web/src/components/ChatDemo.tsx
@@ -2,7 +2,7 @@
 
 import { ChatProvider } from "@chat-sdk/src/context/ChatContext"
 import { ChatLayout } from "@chat-sdk/src/components/ChatLayout"
-
+import { ChatBubble } from "@chat-sdk/src/components/ChatBubble"
 
 export function ChatDemo() {
   return (
@@ -10,6 +10,7 @@ export function ChatDemo() {
       <ChatProvider userId="current-user" token="demo-token" websocketUrl="demo" enableWebSocket={false}>
         <div className="h-full">
           <ChatLayout />
+          <ChatBubble />
         </div>
       </ChatProvider>
     </div>
