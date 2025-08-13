12fe9c2b1da8440a7ab54f86ee0a7248666a219d
sh1ba <sh1ba@sh1bas-MacBook-Pro.local>
2025-08-13 16:01:41 +0700

    chore: add icon

--- packages/chat-sdk/src/index.ts
+++ packages/chat-sdk/src/index.ts
@@ -1,10 +1,13 @@
 import DChatDeskMessage from "./screens/desk-message"
 import { Platform, LogLevel } from "@openim/wasm-client-sdk"
+import { Icon } from "./components/icon"
 // Main exports for the SDK
 export { ChatProvider, useChatContext } from "./context/ChatContext"
 
 //Screens
 export { DChatDeskMessage }
+
+export { Icon }
 
 export * from "./types/sdk"
 
