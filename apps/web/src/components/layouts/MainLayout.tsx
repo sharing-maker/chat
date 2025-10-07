6cfc8be45481a44302c7cd7872fe567985730e82
sh1ba <sh1ba@sh1bas-MacBook-Pro.local>
2025-10-07 09:50:05 +0700

    feat: update env

--- apps/web/src/components/layouts/MainLayout.tsx
+++ apps/web/src/components/layouts/MainLayout.tsx
@@ -34,9 +34,6 @@ export default function MainLayout({ children }: MainLayoutProps) {
   const getSelfInfo = UStore((state) => state.getSelfInfo);
   const { mutate } = useUpdateFcmToken();
   const chatToken = useAuthStore((state) => state.chatToken);
-  const selectedConversationId = useConversationStore(
-    (state) => state.selectedConversationId
-  );
 
   const onGetFcmToken = useCallback(async () => {
     const fcmToken = await getFcmToken();
@@ -58,8 +55,9 @@ export default function MainLayout({ children }: MainLayoutProps) {
             ex = {};
           }
           if (
-            selectedConversationId &&
-            selectedConversationId !== ex?.conversationId
+            useConversationStore?.getState?.()?.selectedConversationId &&
+            useConversationStore?.getState?.()?.selectedConversationId !==
+              ex?.conversationId
           ) {
             const notification = new Notification(ex?.title || "New message", {
               body: ex?.desc,
